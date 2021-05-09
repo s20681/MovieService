c8d4955c62d4efe9f3bc777528f59811e436a44a
s20681 <maciekz16@gmail.com>
2021-05-09 14:50:36 +0200

    cwiczenie 1 done

--- src/main/java/com/s20681/MovieService/Controller/MovieController.java
+++ src/main/java/com/s20681/MovieService/Controller/MovieController.java
@@ -1,13 +1,15 @@
 package com.s20681.MovieService.Controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class MovieController {
 
     @RequestMapping("/exception")
-    void throwException() throws RuntimeException{
-        throw new RuntimeException("tresc_naszego_błędu");
+    void throwException2() throws RuntimeException{
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "tresc_naszego_błędu");
     }
 }
